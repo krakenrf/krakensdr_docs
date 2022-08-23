3a1103c377aab415e6723b554bcb75f2679c025e
rtlsdrblog <rtlsdrblog@gmail.com>
2022-08-23 19:57:35 +1200

    Create krakensdr_x86_install_doa_pr.sh

---
+++ install_scripts/krakensdr_x86_install_doa_pr.sh
@@ -0,0 +1,35 @@
+sudo apt update
+sudo apt install build-essential git cmake libusb-1.0-0-dev lsof libzmq3-dev
+
+git clone https://github.com/krakenrf/librtlsdr
+cd librtlsdr
+mkdir build
+cd build
+cmake ../ -DINSTALL_UDEV_RULES=ON
+make
+sudo make install
+sudo ldconfig
+
+echo 'blacklist dvb_usb_rtl28xxu' | sudo tee --append /etc/modprobe.d/blacklist-dvb_usb_rtl28xxu.conf
+
+
+sudo apt-get install clang
+
+git clone https://github.com/krakenrf/kfr
+cd kfr
+mkdir build
+cd build
+cmake -DENABLE_CAPI_BUILD=ON -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release ..
+make
+
+sudo cp ~/kfr/build/lib/* /usr/local/lib
+sudo mkdir /usr/include/kfr
+sudo cp ~/kfr/include/kfr/capi.h /usr/include/kfr
+sudo ldconfig
+
+cd
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
+chmod ug+x Miniforge3-Linux-x86_64.sh
+./Miniforge3-Linux-x86_64.sh
+
+
