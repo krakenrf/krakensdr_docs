53f0ede5186af2f076f998564f85539bb14fb678
rtlsdrblog <rtlsdrblog@gmail.com>
2022-08-23 20:39:30 +1200

    Update krakensdr_x86_install_doa_pr.sh

--- install_scripts/krakensdr_x86_install_doa_pr.sh
+++ install_scripts/krakensdr_x86_install_doa_pr.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt -y install build-essential git cmake libusb-1.0-0-dev lsof libzmq3-dev clang
+sudo apt -y install build-essential git cmake libusb-1.0-0-dev lsof libzmq3-dev clang php-cli nodejs gpsd
 
 git clone https://github.com/krakenrf/librtlsdr
 cd librtlsdr
@@ -11,7 +11,6 @@ sudo make install
 sudo ldconfig
 
 echo 'blacklist dvb_usb_rtl28xxu' | sudo tee --append /etc/modprobe.d/blacklist-dvb_usb_rtl28xxu.conf
-
 
 git clone https://github.com/krakenrf/kfr
 cd kfr
@@ -29,3 +28,46 @@ cd
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
 chmod ug+x Miniforge3-Linux-x86_64.sh
 ./Miniforge3-Linux-x86_64.sh -b
+
+export PATH=/home/krakenrf/miniforge3/bin/:$PATH
+/home/krakenrf/miniforge3/bin/conda config --set auto_activate_base false
+
+conda create -n kraken python=3.9.7
+conda activate kraken
+
+conda install scipy
+conda install numba
+conda install configparser
+conda install pyzmq
+conda install scikit-rf 
+
+cd
+mkdir krakensdr_doa
+cd krakensdr_doa
+
+git clone https://github.com/krakenrf/heimdall_daq_fw
+cd heimdall_daq_fw
+
+cd ~/krakensdr_doa/heimdall_daq_fw/Firmware/_daq_core/
+make
+
+conda install quart
+conda install pandas
+conda install orjson
+conda install matplotlib
+conda install requests
+
+pip3 install dash_bootstrap_components==1.1.0
+pip3 install quart_compress
+pip3 install dash_devices
+pip3 install pyargus
+
+conda install dash==1.20.0
+conda install werkzeug==2.0.2
+
+pip3 install gpsd-py3
+
+cd ~/krakensdr_doa
+git clone https://github.com/krakenrf/krakensdr_doa
+cp krakensdr_doa/util/kraken_doa_start.sh .
+cp krakensdr_doa/util/kraken_doa_stop.sh .
