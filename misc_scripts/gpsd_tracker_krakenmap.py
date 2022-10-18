32dd4f406019e7cb405fc7c44bab40d90bc7ba05
krakenrf <78108016+krakenrf@users.noreply.github.com>
2022-10-18 23:16:48 +1300

    Create gpsd_tracker_krakenmap.py

---
+++ misc_scripts/gpsd_tracker_krakenmap.py
@@ -0,0 +1,35 @@
+# Uses an attached USB GPS and GPSd to get location data, and then upload it as a marker to the Kraken Cloud Mapper.
+
+import gpsd
+import time
+import requests
+
+gpsd.connect()
+time.sleep(2)
+
+API_SERVER = 'https://map.krakenrf.com:443'
+login = {'username': 'username', 'password': 'password'}
+
+while(1):
+    try:
+        x = requests.post(API_SERVER + '/login', json = login)
+        break
+    except:
+        time.sleep(1)
+        pass
+
+token = x.text
+
+while(1):
+    try:
+        packet = gpsd.get_current()
+        lat, lon = packet.position()
+        print("lat: " + str(lat))
+        print("lon: " + str(lon))
+
+        beaconData = {'lat': lat, 'lon': lon, 'speed': 0, 'height': 0}
+        x = requests.post(API_SERVER + '/beacon', json = beaconData, headers = {'Authorization': token})
+
+    except (gpsd.NoFixError, UserWarning):
+        print("waiting for fix")
+    time.sleep(1)
